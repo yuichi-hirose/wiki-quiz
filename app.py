f4ae81ee820c32f0005bdd470cb8a6b1713d7f7c
Hirose Yuichi <moa.poketto@ezweb.ne.jp>
2022-06-19 03:43:03 +0900

    all fixed

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # インポートするライブラリ
-from msilib.schema import Error
 from flask import Flask, request, abort
 
 from linebot import (
@@ -51,6 +50,19 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #profile = line_bot_api.get_profile(event.source.user_id)
+    """
+    status_msg = profile.status_message
+    if status_msg != "None":
+        # LINEに登録されているstatus_messageが空の場合は、"なし"という文字列を代わりの値とする
+        status_msg = "なし"
+    status_message = TemplateSendMessage(alt_text="Buttons template",
+                                    template=ButtonsTemplate(
+                                    thumbnail_image_url=profile.picture_url,
+                                   title=profile.display_name,
+                                   text=f"User Id: {profile.user_id[:5]}...\n"
+                                        f"Status Message: {status_msg}",
+                                   actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
+    """
     #userid=profile.user_id
     userid=event.source.user_id
     answering=False
@@ -127,14 +139,14 @@ def handle_message(event):
             
 
         else:
-            message=f"受け取った入力は{event.message.text}\nクイズがしたいときは「クイズ」と入力してね！"
+            message="「クイズ」と入力してね！"
             messages.append(TextSendMessage(text=message))
-
+    
     line_bot_api.reply_message(
         event.reply_token,
         messages
     )
-
+    
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))

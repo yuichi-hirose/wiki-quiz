8a77eab4986524f121fea45a37f0afe1492f29e3
Hirose Yuichi <moa.poketto@ezweb.ne.jp>
2022-06-18 20:39:06 +0900

    refined

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-# ライブラリをインポート
+# インポートするライブラリ
 from flask import Flask, request, abort
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,41 +8,47 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage
+    FollowEvent, MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction
 )
 import os
-#Flaskを準備
+
+# 軽量なウェブアプリケーションフレームワーク:Flask
 app = Flask(__name__)
+
+
 #環境変数からLINE Access Tokenを設定
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 #環境変数からLINE Channel Secretを設定
 LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
-#LineBotApiのインスタンスを生成
+
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-#WebhookHandlerのインスタンスを生成
-webhook_handler = WebhookHandler(LINE_CHANNEL_SECRET)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    # HTTPリクエストヘッダからX-Line-Signatureを取り出す
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    #テキストでpostされたデータを取得
+
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # webhookのbodyを解析する
-    #この結果はadd関数で受け取る
-    #なお、Signatureが一致していない時はInvalidSignatureError例外が発生する
+
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
+
 # MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #テキストでの返信を行う
-    line_bot_api.reply_message(
+	line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text )
-    )
+        TextSendMessage(text='「' + event.message.text + '」って何？')
+     )
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)

699b213e369debaf69c4b043e754376e2ef953c6
Hirose Yuichi <moa.poketto@ezweb.ne.jp>
2022-06-19 03:27:48 +0900

    catch exception

--- app.py
+++ app.py
@@ -51,19 +51,6 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #profile = line_bot_api.get_profile(event.source.user_id)
-    """
-    status_msg = profile.status_message
-    if status_msg != "None":
-        # LINEに登録されているstatus_messageが空の場合は、"なし"という文字列を代わりの値とする
-        status_msg = "なし"
-    status_message = TemplateSendMessage(alt_text="Buttons template",
-                                    template=ButtonsTemplate(
-                                    thumbnail_image_url=profile.picture_url,
-                                   title=profile.display_name,
-                                   text=f"User Id: {profile.user_id[:5]}...\n"
-                                        f"Status Message: {status_msg}",
-                                   actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
-    """
     #userid=profile.user_id
     try:
         userid=event.source.user_id
@@ -148,7 +135,7 @@ def handle_message(event):
             event.reply_token,
             messages
         )
-    except Error as e:
+    except Exception as e:
         message=str(e)
         messages.append(TextSendMessage(text=message))
         line_bot_api.reply_message(

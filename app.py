46641f37ea1d8b5d39c2dbec5a2bebb4c735a544
Hirose Yuichi <moa.poketto@ezweb.ne.jp>
2022-06-19 03:52:07 +0900

    fixed randint error

--- app.py
+++ app.py
@@ -50,19 +50,6 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #profile = line_bot_api.get_profile(event.source.user_id)
-    """
-    status_msg = profile.status_message
-    if status_msg != "None":
-        # LINEに登録されているstatus_messageが空の場合は、"なし"という文字列を代わりの値とする
-        status_msg = "なし"
-    status_message = TemplateSendMessage(alt_text="Buttons template",
-                                    template=ButtonsTemplate(
-                                    thumbnail_image_url=profile.picture_url,
-                                   title=profile.display_name,
-                                   text=f"User Id: {profile.user_id[:5]}...\n"
-                                        f"Status Message: {status_msg}",
-                                   actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
-    """
     #userid=profile.user_id
     userid=event.source.user_id
     answering=False
@@ -126,7 +113,7 @@ def handle_message(event):
             with open("quiz_data.csv","r") as f:
                 reader = csv.reader(f)
                 list_reader=list(reader)[1:]
-                idx=random.randint(0,len(list_reader))
+                idx=random.randint(0,len(list_reader)-1)
                 question=list_reader[idx]
             
             message2="最初のヒントは\n"+question[1]

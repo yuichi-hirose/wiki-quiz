aeec30626ff18bcdfa21f9d4bb0a19b2134d380f
Hirose Yuichi <moa.poketto@ezweb.ne.jp>
2022-06-19 11:49:24 +0900

    test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # インポートするライブラリ
+from tokenize import group
 from flask import Flask, request, abort
 
 from linebot import (
@@ -54,98 +55,15 @@ def handle_message(event):
     #b=time.time()
     #profile = line_bot_api.get_profile(event.source.user_id)
     #userid=profile.user_id
+    message_type=event.source.type
     userid=event.source.user_id
-    
-
-    con = sqlite3.connect('cache.db')
-    cur = con.cursor()
-    #df = pd.read_csv("cache.csv")
-    #user_data=df[df['userid'] ==userid]
-    user_sql=cur.execute(f"SELECT * FROM cache WHERE userid = '{userid}'")
-    answering=False
-    for u in user_sql:
-        user_data=u  #tuple
-        answering=True
-
-    # answering=len(user_data)>0
-
     messages=[]
-
-    if(event.message.text=="ステータス"):
-        message=str(userid)+"\n"+str(answering)
-        status_message=TextSendMessage(text=message)
-        messages.append(status_message)
-        all_data=cur.execute(f"SELECT * FROM cache")
-        message=""
-        for d in all_data:
-            message+=str(d)+"\n"
-        
-        list_message=TextSendMessage(text=message)
-        messages.append(list_message)
-    elif(answering): #judge
-        
-        ans=user_data[1]
-        hints=user_data[2:]
-        if(event.message.text==ans):
-            message="正解！"
-            judge_message=TextSendMessage(text=message)
-            messages.append(judge_message)
-            #キャッシュを消去
-            cur.execute(f"DELETE FROM cache WHERE userid='{userid}'")
-            con.commit()
-            con.close()
-            #df=df.drop(user_idx)
-            #df.to_csv('cache.csv', index=False)
-        else:
-            message="不正解！"
-            judge_message=TextSendMessage(text=message)
-            messages.append(judge_message)
-            next_idx=0
-
-            for i,hint in enumerate(hints):
-                if(hint is None):
-                    pass
-                else:
-                    next_hint=hint
-                    next_idx=i
-
-                    message=f"次のヒントは\n{next_hint}"
-                    hint_message=TextSendMessage(text=message)
-                    messages.append(hint_message)
-                    
-                    cur.execute(f"UPDATE cache SET hint{next_idx+1}=NULL WHERE userid='{userid}'")
-                    con.commit()
-                    con.close()
-                    break
-            else:  #all hints are nan
-                message=f"答えは{ans}でした！"
-                messages.append(TextSendMessage(text=message))
-                cur.execute(f"DELETE FROM cache WHERE userid='{userid}'")
-                con.commit()
-                con.close()
-            
-
-    else:  #select question
-        if(event.message.text=="クイズ"):
-            message1="問題"
-            messages.append(TextSendMessage(text=message1))
-            #q,hints=quiz.generate_quiz("織田信長")
-            with open("quiz_data.csv","r") as f:
-                reader = csv.reader(f)
-                list_reader=list(reader)[1:]
-                idx=random.randint(0,len(list_reader)-1)
-                question=list_reader[idx]
-            
-            message2="最初のヒントは\n"+question[1]
-            messages.append(TextSendMessage(text=message2))
-
-            cur.execute(f"INSERT INTO cache VALUES ('{userid}','{question[0]}','{question[2]}','{question[3]}','{question[4]}','{question[5]}')")
-            con.commit()
-            con.close()
-        else:
-            message=f"受け取った入力は「{event.message.text}」\n"
-            message+="「クイズ」と入力してね！"
-            messages.append(TextSendMessage(text=message))
+    if(message_type=="group"):
+        groupid=event.source.group_id
+        message=f"type:{message_type}, group id: {groupid}, user id: {userid}"
+    else:
+        message=f"type:{message_type}, user id: {userid}"
+    messages.append(TextSendMessage(text=message))
     
     #messages.append(TextSendMessage(text=str(time.time()-b)))
     line_bot_api.reply_message(
